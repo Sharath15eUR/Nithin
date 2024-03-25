fb21bf2fecd706f3455ed03bf8d75ee1c5c8ab62
Nithin Raju <98793345+nithin-06@users.noreply.github.com>
2024-03-25 15:59:12 +0530

    Add files via upload

---
+++ Week 4/TCP_Client.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define PORT 8080
+#define BUFFER_SIZE 1024
+
+int main(int argc, char const *argv[]) {
+    int sock = 0, valread;
+    struct sockaddr_in serv_addr;
+    char buffer[BUFFER_SIZE] = {0};
+    char input_string[BUFFER_SIZE];
+
+    printf("Enter a string: ");
+    fgets(input_string, BUFFER_SIZE, stdin);
+
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        printf("\n Socket creation error \n");
+        return -1;
+    }
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+
+    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0) {
+        printf("\nInvalid address/ Address not supported \n");
+        return -1;
+    }
+
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        printf("\nConnection Failed \n");
+        return -1;
+    }
+
+    send(sock , input_string , strlen(input_string) , 0 );
+    printf("String sent to server\n");
+    valread = read( sock , buffer, BUFFER_SIZE);
+    printf("Server response: %s\n",buffer );
+    return 0;
+}
